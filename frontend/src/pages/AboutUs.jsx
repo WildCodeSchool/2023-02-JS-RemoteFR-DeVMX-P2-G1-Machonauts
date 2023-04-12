11c4de0cd917df2eee94725ff14f770b959fbadd
Lopes93600 <120326900+Lopes93600@users.noreply.github.com>
2023-04-12 15:33:34 +0200

    add aboutUs scss in sass and import in styles.scss

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -1,23 +1,29 @@
 function AboutUs() {
   return (
-    <div>
-      <h1>Qui sommes nous</h1>
-      <p>
-        Nous sommes sommes une entrprise qui vous permettra de voyager dans
-        l'espace.
-      </p>
-      <h2>Our Mission</h2>
-      <p>
-        Notre mission est de vous apporter toutes les informations utiles pour
-        pouvoir vous immerger dans cet univers .
-      </p>
-      <h2>Notre équipe</h2>
-      <ul>
-        <li>Damien - Frontend Developer</li>
-        <li>Victor - Backend Developer</li>
-        <li>Jean-baptiste - Full Stack Developer</li>
-        <li>Nicolas - Full Stack Developer</li>
-      </ul>
+    <div className="aboutUs-container-global">
+      <div className="entreprise-infos-container">
+        <h1>Qui sommes nous</h1>
+        <p>
+          Nous sommes sommes une entrprise qui vous permettra de voyager dans
+          l'espace.
+        </p>
+      </div>
+      <div className="mission-infos-container">
+        <h2>Our Mission</h2>
+        <p>
+          Notre mission est de vous apporter toutes les informations utiles pour
+          pouvoir vous immerger dans cet univers .
+        </p>
+      </div>
+      <div className="team-infos-container">
+        <h2>Notre équipe</h2>
+        <ul>
+          <li>Damien - Frontend Developer</li>
+          <li>Victor - Backend Developer</li>
+          <li>Jean-baptiste - Full Stack Developer</li>
+          <li>Nicolas - Full Stack Developer</li>
+        </ul>
+      </div>
     </div>
   );
 }
