c0218b606332ce0bca1d02ff76698c82c631f26c
jean-baptiste NOIRAUD <jean.noiraud@gmail.com>
2023-04-14 10:55:09 +0200

    planets appears correctly and box-shadow color changed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,56 +3,56 @@ function Home() {
     <div className="home">
       <a href="mercury">
         <img
-          src="./src/assets/planets-surface/mercury.jpg"
+          src="../src/assets/planets-surface/mercury.jpg"
           className="mercury"
           alt="Mercure"
         />
       </a>
       <a href="venus">
         <img
-          src="./src/assets/planets-surface/venus.jpg"
+          src="../src/assets/planets-surface/venus.jpg"
           className="venus"
           alt="Venus"
         />
       </a>
       <a href="earth">
         <img
-          src="./src/assets/planets-surface/earth.jpg"
+          src="../src/assets/planets-surface/earth.jpg"
           className="earth"
           alt="Terre"
         />
       </a>
       <a href="mars">
         <img
-          src="./src/assets/planets-surface/mars.jpg"
+          src="../src/assets/planets-surface/mars.jpg"
           className="mars"
           alt="Mars"
         />
       </a>
       <a href="jupiter">
         <img
-          src="./src/assets/planets-surface/jupiter.jpg"
+          src="../src/assets/planets-surface/jupiter.jpg"
           className="jupiter"
           alt="Jupiter"
         />
       </a>
       <a href="saturne">
         <img
-          src="./src/assets/planets-surface/saturne.jpg"
+          src="../src/assets/planets-surface/saturne.jpg"
           className="saturne"
           alt="Saturne"
         />
       </a>
       <a href="uranus">
         <img
-          src="./src/assets/planets-surface/uranus.jpg"
+          src="../src/assets/planets-surface/uranus.jpg"
           className="uranus"
           alt="Uranus"
         />
       </a>
       <a href="neptune">
         <img
-          src="./src/assets/planets-surface/neptune.jpg"
+          src="../src/assets/planets-surface/neptune.jpg"
           className="neptune"
           alt="Neptune"
         />
