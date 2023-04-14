6689e1e869a54423975afae9751ab8257810652c
jean-baptiste NOIRAUD <jean.noiraud@gmail.com>
2023-04-14 10:41:29 +0200

    change to link for the planet component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,46 +1,62 @@
 function Home() {
   return (
     <div className="home">
-      <img
-        src="./src/assets/planets-surface/mercury.jpg"
-        className="mercury"
-        alt="Mercure"
-      />
-      <img
-        src="./src/assets/planets-surface/venus.jpg"
-        className="venus"
-        alt="Venus"
-      />
-      <img
-        src="./src/assets/planets-surface/earth.jpg"
-        className="earth"
-        alt="Terre"
-      />
-      <img
-        src="./src/assets/planets-surface/mars.jpg"
-        className="mars"
-        alt="Mars"
-      />
-      <img
-        src="./src/assets/planets-surface/jupiter.jpg"
-        className="jupiter"
-        alt="Jupiter"
-      />
-      <img
-        src="./src/assets/planets-surface/saturne.jpg"
-        className="saturne"
-        alt="Saturne"
-      />
-      <img
-        src="./src/assets/planets-surface/uranus.jpg"
-        className="uranus"
-        alt="Uranus"
-      />
-      <img
-        src="./src/assets/planets-surface/neptune.jpg"
-        className="neptune"
-        alt="Neptune"
-      />
+      <a href="mercury">
+        <img
+          src="./src/assets/planets-surface/mercury.jpg"
+          className="mercury"
+          alt="Mercure"
+        />
+      </a>
+      <a href="venus">
+        <img
+          src="./src/assets/planets-surface/venus.jpg"
+          className="venus"
+          alt="Venus"
+        />
+      </a>
+      <a href="earth">
+        <img
+          src="./src/assets/planets-surface/earth.jpg"
+          className="earth"
+          alt="Terre"
+        />
+      </a>
+      <a href="mars">
+        <img
+          src="./src/assets/planets-surface/mars.jpg"
+          className="mars"
+          alt="Mars"
+        />
+      </a>
+      <a href="jupiter">
+        <img
+          src="./src/assets/planets-surface/jupiter.jpg"
+          className="jupiter"
+          alt="Jupiter"
+        />
+      </a>
+      <a href="saturne">
+        <img
+          src="./src/assets/planets-surface/saturne.jpg"
+          className="saturne"
+          alt="Saturne"
+        />
+      </a>
+      <a href="uranus">
+        <img
+          src="./src/assets/planets-surface/uranus.jpg"
+          className="uranus"
+          alt="Uranus"
+        />
+      </a>
+      <a href="neptune">
+        <img
+          src="./src/assets/planets-surface/neptune.jpg"
+          className="neptune"
+          alt="Neptune"
+        />
+      </a>
     </div>
   );
 }
