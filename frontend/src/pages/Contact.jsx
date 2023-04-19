0e8400d8185d693efc414ff39459e437bff00af9
Damien Arnaud <damien.arnaudpro@gmail.com>
2023-04-19 18:02:00 +0200

    Validation ok

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -6,48 +6,19 @@ function Contact() {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   const [changeClass, setChangeClass] = useState("send-form-off");
-  const myErrorFstN = document.getElementById("errorFstN");
-  const myErrorLstN = document.getElementById("errorLstN");
-  const myErrorEm = document.getElementById("errorEm");
-  const myErrorMes = document.getElementById("errorMes");
-  const myRegex = /^[a-zA-Z-\s]+$/;
 
   /* Validation Form*************************** */
 
   function submit(e) {
-    if (firstName.trim() === "") {
-      e.preventDefault();
-      myErrorFstN.innerHTML = "First Name is required ";
-    } else if (myRegex.test(firstName) === false) {
-      e.preventDefault();
-      myErrorFstN.innerHTML = "First Name need letter an - only ! ";
-    } else if (lastName.trim() === "") {
-      e.preventDefault();
-      myErrorLstN.innerHTML = "Last Name is required ";
-    } else if (myRegex.test(lastName) === false) {
-      e.preventDefault();
-      myErrorLstN.innerHTML = "Last Name need letter an - only ! ";
-    } else if (email.trim() === "") {
-      e.preventDefault();
-      myErrorEm.innerHTML = "Email is required ";
-    } else if (myRegex.test(email) === false) {
-      e.preventDefault();
-      myErrorEm.innerHTML = "Email need letter an - only ! ";
-    } else if (message.trim() === "") {
-      e.preventDefault();
-      myErrorMes.innerHTML = "Message is required ";
-    } else if (myRegex.test(message) === false) {
-      e.preventDefault();
-      myErrorMes.innerHTML = "Message need letter an - only ! ";
-    }
-    return true;
+    e.preventDefault();
+    setChangeClass("send-form-on");
   }
 
   /* Form **************************** */
 
   return (
     <div className="contact-page">
-      <form className="contact-form" id="my_Form" onSubmit={() => submit()}>
+      <form className="contact-form" id="my_Form" onSubmit={submit}>
         <h1 className="h1-Contact">Contact Us</h1>
         <label>
           <input
@@ -61,7 +32,6 @@ function Contact() {
             }}
             required
           />
-          <span id="errorFstN"> </span>
         </label>
         <label>
           <input
@@ -73,7 +43,6 @@ function Contact() {
             onChange={(e) => setLastName(e.target.value)}
             required
           />
-          <span id="errorLstN"> </span>
         </label>
         <label>
           <input
@@ -85,7 +54,6 @@ function Contact() {
             onChange={(e) => setEmail(e.target.value)}
             required
           />
-          <span id="errorEm"> </span>
         </label>
         <textarea
           id="text_area"
@@ -95,18 +63,8 @@ function Contact() {
           value={message}
           required
         />
-        <span id="errorMes"> </span>
 
-        <button
-          className="buttonContact"
-          id="button_contact"
-          type="submit"
-          onClick={() => {
-            if (submit === true) {
-              setChangeClass("send-form-on");
-            }
-          }}
-        >
+        <button className="buttonContact" id="button_contact" type="submit">
           Send
         </button>
 
@@ -123,7 +81,6 @@ function Contact() {
               setEmail("");
               setFirstName("");
               setMessage("");
-              myErrorFstN.innerHTML = " ";
             }}
           >
             &#128640;
