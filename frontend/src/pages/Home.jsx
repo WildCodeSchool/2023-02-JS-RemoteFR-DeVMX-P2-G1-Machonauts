93382c18a623eb20ce58d06b19ca15e615417817
jean-baptiste NOIRAUD <jean.noiraud@gmail.com>
2023-04-19 11:00:20 +0200

    name added but not good center, have to fix

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,70 +1,102 @@
 function Home() {
   return (
     <div className="home">
-      <a href="mercury">
-        <img
-          src="../src/assets/planets-surface/mercury.jpg"
-          className="mercury"
-          alt="Mercure"
-        />
-        <h1>Mercure</h1>
-      </a>
-      <a href="venus">
-        <img
-          src="../src/assets/planets-surface/venus.jpg"
-          className="venus"
-          alt="Venus"
-        />
-        <h1>Venus</h1>
-      </a>
-      <a href="/pages/Planet">
-        <img
-          src="../src/assets/planets-surface/earth.jpg"
-          className="earth"
-          alt="Terre"
-        />
-        <h1>Terre</h1>
-      </a>
-      <a href="mars">
-        <img
-          src="../src/assets/planets-surface/mars.jpg"
-          className="mars"
-          alt="Mars"
-        />
-        <h1>Mars</h1>
-      </a>
-      <a href="jupiter">
-        <img
-          src="../src/assets/planets-surface/jupiter.jpg"
-          className="jupiter"
-          alt="Jupiter"
-        />
-        <h1>Jupiter</h1>
-      </a>
-      <a href="saturne">
-        <img
-          src="../src/assets/planets-surface/saturne.jpg"
-          className="saturne"
-          alt="Saturne"
-        />
-        <h1>Saturne</h1>
-      </a>
-      <a href="uranus">
-        <img
-          src="../src/assets/planets-surface/uranus.jpg"
-          className="uranus"
-          alt="Uranus"
-        />
-        <h1>Uranus</h1>
-      </a>
-      <a href="neptune">
-        <img
-          src="../src/assets/planets-surface/neptune.jpg"
-          className="neptune"
-          alt="Neptune"
-        />
-        <h1>Neptune</h1>
-      </a>
+      <div className="planets-pic">
+        <a href="mercury">
+          <img
+            src="../src/assets/planets-surface/mercury.jpg"
+            className="planet-img"
+            alt="Mercure"
+          />
+        </a>
+        <div>
+          <h1>Mercure</h1>
+        </div>
+      </div>
+      <div className="planets-pic">
+        <a href="venus">
+          <img
+            src="../src/assets/planets-surface/venus.jpg"
+            className="planet-img"
+            alt="Venus"
+          />
+        </a>
+        <div>
+          <h1>Venus</h1>
+        </div>
+      </div>
+      <div className="planets-pic">
+        <a href="/pages/Planet">
+          <img
+            src="../src/assets/planets-surface/earth.jpg"
+            className="planet-img"
+            alt="Terre"
+          />
+        </a>
+        <div>
+          <h1>Terre</h1>
+        </div>
+      </div>
+      <div className="planets-pic">
+        <a href="mars">
+          <img
+            src="../src/assets/planets-surface/mars.jpg"
+            className="planet-img"
+            alt="Mars"
+          />
+        </a>
+        <div>
+          <h1>Mars</h1>
+        </div>
+      </div>
+      <div className="planets-pic">
+        <a href="jupiter">
+          <img
+            src="../src/assets/planets-surface/jupiter.jpg"
+            className="planet-img"
+            alt="Jupiter"
+          />
+        </a>
+        <div>
+          <h1>Jupiter</h1>
+        </div>
+      </div>
+      <div className="planets-pic">
+        <a href="saturne">
+          <img
+            src="../src/assets/planets-surface/saturne.jpg"
+            className="planet-img"
+            alt="Saturne"
+          />
+        </a>
+        <div>
+          <h1>Saturne</h1>
+        </div>
+      </div>
+      <div className="planets-pic">
+        <a href="uranus">
+          <img
+            src="../src/assets/planets-surface/uranus.jpg"
+            className="planet-img"
+            alt="Uranus"
+          />
+        </a>
+        <div>
+          <h1>Uranus</h1>
+        </div>
+      </div>
+      <div className="planets-pic">
+        <a href="neptune">
+          <img
+            src="../src/assets/planets-surface/neptune.jpg"
+            className="planet-img"
+            alt="Neptune"
+          />
+        </a>
+        <div>
+          <h1>Neptune</h1>
+        </div>
+      </div>
     </div>
   );
 }
