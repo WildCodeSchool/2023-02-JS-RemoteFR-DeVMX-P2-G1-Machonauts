554ebdf537e834501e4b8a589a25d5925f0d89d0
Lopes93600 <120326900+Lopes93600@users.noreply.github.com>
2023-04-19 12:53:57 +0200

    style update with variables and update style team card

--- frontend/src/datas/data-infos-profils.js
+++ frontend/src/datas/data-infos-profils.js
@@ -4,7 +4,8 @@ const profils = [
     firstName: "Nicolas",
     lastName: "Lopes",
     title: "Spécialiste communication",
-    description: "",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi autodit accusamus saepe nesciunt culpa distinctio voluptatibus, alias eaquisquam nemo qui hic consequuntur obcaecati voluptate ipsamrecusandae asperiores vel.",
     linkedin: "https://www.linkedin.com/in/nicolas-lopes-21441478",
     github: "https://github.com/SEF936",
     mail: "lopes_nico@yahoo.fr",
@@ -14,7 +15,8 @@ const profils = [
     firstName: "Damien",
     lastName: "Arnaud",
     title: "Pilote spatial",
-    description: "",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi autodit accusamus saepe nesciunt culpa distinctio voluptatibus, alias eaquisquam nemo qui hic consequuntur obcaecati voluptate ipsamrecusandae asperiores vel.",
     linkedin: "https://www.linkedin.com/in/damien-a-5a303660/",
     github: "https://github.com/Zanelidd",
     mail: "",
@@ -24,7 +26,9 @@ const profils = [
     firstName: "Jean-baptiste",
     lastName: "Noiraud",
     title: "Astronome",
-    description: "",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi autodit accusamus saepe nesciunt culpa distinctio voluptatibus, alias eaquisquam nemo qui hic consequuntur obcaecati voluptate ipsamrecusandae asperiores vel.",
+
     linkedin: "",
     github: "https://github.com/jbaptiste51",
     mail: "",
@@ -34,7 +38,9 @@ const profils = [
     firstName: "Victor",
     lastName: "Brito",
     title: "Spécialiste technique",
-    description: "",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi autodit accusamus saepe nesciunt culpa distinctio voluptatibus, alias eaquisquam nemo qui hic consequuntur obcaecati voluptate ipsamrecusandae asperiores vel.",
+
     linkedin: "https://www.linkedin.com/in/victor-brito-69040a191",
     github: "https://github.com/webdevbynight",
     mail: "",
