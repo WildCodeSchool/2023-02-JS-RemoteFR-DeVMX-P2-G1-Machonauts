dcaf677b55b70b5e1d87c4758c99743aac34337e
Lopes93600 <120326900+Lopes93600@users.noreply.github.com>
2023-04-19 10:08:38 +0200

    create file with data profils

---
+++ frontend/src/datas/data-infos-profils.js
@@ -0,0 +1,40 @@
+const profils = [
+  {
+    firstName: "Nicolas",
+    lastName: "Lopes",
+    title: "Spécialiste communication",
+    description: "",
+    linkedin: "https://www.linkedin.com/in/nicolas-lopes-21441478",
+    github: "https://github.com/SEF936",
+    mail: "",
+  },
+  {
+    firstName: "Damien",
+    lastName: "Arnaud",
+    title: "Pilote spacial",
+    description: "",
+    linkedin: "https://www.linkedin.com/in/damien-a-5a303660/",
+    github: "https://github.com/Zanelidd",
+    mail: "",
+  },
+  {
+    firstName: "Jean-baptiste",
+    lastName: "Noiraud",
+    title: "Astronome",
+    description: "",
+    linkedin: "",
+    github: "https://github.com/jbaptiste51",
+    mail: "",
+  },
+  {
+    firstName: "Victor",
+    lastName: "Brito",
+    title: "Spécialiste technique",
+    description: "",
+    linkedin: "https://www.linkedin.com/in/victor-brito-69040a191",
+    github: "https://github.com/webdevbynight",
+    mail: "",
+  },
+];
+
+export default profils;
