e12ef30cc0b34e24584c655b52a732abf68634e9
webdevbynight <contact@victor-brito.name>
2023-04-19 10:52:03 +0200

    Changed a HTML native elements to Link React Router DOM elements

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,62 +1,64 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
     <div className="home">
-      <a href="/pages/Planet/mercury">
+      <Link to="/pages/Planet/mercury">
         <img
           src="../src/assets/planets-surface/mercury.jpg"
           className="mercury"
           alt="Mercure"
         />
-      </a>
-      <a href="/pages/Planet/venus">
+      </Link>
+      <Link to="/pages/Planet/venus">
         <img
           src="../src/assets/planets-surface/venus.jpg"
           className="venus"
           alt="Venus"
         />
-      </a>
-      <a href="/pages/Planet/earth">
+      </Link>
+      <Link to="/pages/Planet/earth">
         <img
           src="../src/assets/planets-surface/earth.jpg"
           className="earth"
           alt="Terre"
         />
-      </a>
-      <a href="/pages/Planet/mars">
+      </Link>
+      <Link to="/pages/Planet/mars">
         <img
           src="../src/assets/planets-surface/mars.jpg"
           className="mars"
           alt="Mars"
         />
-      </a>
-      <a href="/pages/Planet/jupiter">
+      </Link>
+      <Link to="/pages/Planet/jupiter">
         <img
           src="../src/assets/planets-surface/jupiter.jpg"
           className="jupiter"
           alt="Jupiter"
         />
-      </a>
-      <a href="/pages/Planet/saturne">
+      </Link>
+      <Link to="/pages/Planet/saturne">
         <img
           src="../src/assets/planets-surface/saturne.jpg"
           className="saturne"
           alt="Saturne"
         />
-      </a>
-      <a href="/pages/Planet/uranus">
+      </Link>
+      <Link to="/pages/Planet/uranus">
         <img
           src="../src/assets/planets-surface/uranus.jpg"
           className="uranus"
           alt="Uranus"
         />
-      </a>
-      <a href="/pages/Planet/neptune">
+      </Link>
+      <Link to="/pages/Planet/neptune">
         <img
           src="../src/assets/planets-surface/neptune.jpg"
           className="neptune"
           alt="Neptune"
         />
-      </a>
+      </Link>
     </div>
   );
 }
