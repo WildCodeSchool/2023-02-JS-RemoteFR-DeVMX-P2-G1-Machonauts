3a6f0f52c9eb33427b6b3a0443b54cc91319e96a
Lopes93600 <120326900+Lopes93600@users.noreply.github.com>
2023-04-26 10:20:09 +0200

    delete fetchj for rocket and create component rocket for map

---
+++ frontend/src/components/Rocket.jsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line react/prop-types
+function Rocket({ src, description, name, alt }) {
+  return (
+    <div className="rockets-container-global">
+      <div className="rocket-container">
+        <img src={src} alt={alt} />
+        <div className="rocket-infos-container">
+          <h2>{name}</h2>
+          <p>{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Rocket;
